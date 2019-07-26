19c55971129ad617f1b41fddcd779d242493cd7f
Siong Leng <siongleng@ich-group.com>
2019-07-26 21:45:47 +0800

    Select bug fix

--- src/controls/FormControl.tsx
+++ src/controls/FormControl.tsx
@@ -429,7 +429,7 @@ export class FormControl extends React.Component<IProps, IState> {
           // defaultMenuIsOpen
           isDisabled={this.props.disabled}
           className={'select'}
-          value={Options.filter((obj: any) => obj.value.toString() === (this.state.value ? this.state.value.toString() : ''))[0] || ''}
+          value={Options.filter((obj: any) => obj.value.toString() === ((this.state.value !== null && this.state.value !== undefined) ? this.state.value.toString() : ''))[0] || ''}
           placeholder={this.props.placeholder}
           onChange={this.onSetOption}
           options={this.props.selectOptions}
@@ -472,7 +472,7 @@ export class FormControl extends React.Component<IProps, IState> {
           isDisabled={this.props.disabled}
           className={'select'}
           // defaultMenuIsOpen
-          value={Options.filter((obj: any) => obj.value === this.state.value)[0] || ''}
+          value={Options.filter((obj: any) => obj.value.toString() === ((this.state.value !== null && this.state.value !== undefined) ? this.state.value.toString() : ''))[0] || ''}
           placeholder={this.props.placeholder}
           onChange={this.onSetOption}
           components={{ Option: CustomOption, SingleValue: DisplayOption }}
