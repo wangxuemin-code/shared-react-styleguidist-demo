316fec02b943e41b1c1d79f828762e9b4bedfd82
Siong Leng <siongleng@ich-group.com>
2019-08-03 00:07:52 +0800

    Remove redundant generated files

--- build/helpers/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './Assessor';
-export * from './DateTime';
-export * from './Formatter';
-export * from './Mqtt';
-export * from './ApiHelper';
-export * from './ErrorHandle';
-export * from './Cookie';
